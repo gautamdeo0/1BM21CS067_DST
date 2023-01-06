e41d55390fca1d6fff42cfc874e6df408bc0df4a
gautamdeo0 <117442395+gautamdeo0@users.noreply.github.com>
2023-01-06 14:44:57 +0530

    Add files via upload

---
+++ sorting LinkedList.c
@@ -0,0 +1,98 @@
+#include<conio.h>
+#include<stdio.h>
+#include<stdlib.h>
+typedef struct node
+{
+     int info;
+     struct node *next;
+}nodetype;
+nodetype* insert(nodetype*);
+void sort(nodetype*);
+
+nodetype* insert(nodetype *r)
+{
+     int n;
+     nodetype *p;
+     p=(nodetype*)malloc(sizeof(nodetype));
+     printf("\nEnter the number: ");
+     scanf("%d",&n);
+     if(p!=NULL)
+     {
+          p->info=n;
+          p->next=NULL;
+          if(r==NULL)
+          {
+               r=p;
+          }
+          else
+          {
+               r->next=p;
+               r=p;
+          }
+          return(r);
+     }
+     else
+     {
+          printf("not enough memory");
+     }
+}
+void sort(nodetype *l)
+{
+     nodetype *t;
+     nodetype *s;
+     int x;
+     t=l;
+     while(t!=NULL)
+     {
+          s=t->next;
+          while(s!=NULL)
+          {
+               if(t->info>s->info)
+               {
+                    x=t->info;
+                    t->info=s->info;
+                    s->info=x;
+               }
+               s=s->next;
+          }
+          t=t->next;
+      }
+      t=l;
+      while(t!=NULL)
+      {
+            printf(" %d ",t->info);
+            t=t->next;
+      }
+}
+
+void main()
+{
+     nodetype* left=NULL;
+     nodetype* right=NULL;
+     int ch;
+     printf("Enter 1 for insert \nEnter 2 for sort \nEnter 3 for exit");
+     do
+     {
+          printf("\nEnter choice: ");
+          scanf("%d",&ch);
+          switch(ch)
+          {
+               case 1:
+               right=insert(right);
+               if(left==NULL)
+               {
+                   left=right;
+               }
+               break;
+               case 2:
+               sort(left);
+               case 3:
+               exit(1);
+               default:
+               printf("invalid choice");
+               break;
+          }
+          
+     }while(1);
+     getch();
+}
